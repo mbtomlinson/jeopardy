8d994b3ea2e854e1920850bf1ff57da741c1bf54
Matt_Tomlinson <mbrucetomlinson@gmail.com>
2017-01-15 16:47:05 -0500

    experimenting with views for jeopardy data

--- jeopardy/urls.py
+++ jeopardy/urls.py
@@ -4,5 +4,11 @@ from . import views
 app_name = 'jeopardy'
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    # ex: /jeopardy/5/
+    url(r'^(?P<clue_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /jeopardy/5/results/
+    url(r'^(?P<clue_id>[0-9]+)/result/$', views.result, name='result'),
+    # ex: /jeopardy/5/answer/
+    url(r'^(?P<clue_id>[0-9]+)/vote/$', views.answer, name='answer'),
     ]
 
--- jeopardy/views.py
+++ jeopardy/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
+from .models import Clue
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    clue_list = Clue.objects.order_by('-id')[:20]
+    context = {'clue_list': clue_list}
+    return render(request, 'jeopardy/index.html', context)
+
+def detail(request, clue_id):
+    clue = get_object_or_404(Clue, pk=clue_id)
+    return render(request, 'jeopardy/detail.html', {'clue': clue})
+
+def result(request, clue_id):
+    return HttpResponse(response % question_id)
+
+def answer(request, clue_id):
+    return HttpResponse("You're trying to answer clue %s." % clue_id)
