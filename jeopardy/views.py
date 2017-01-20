ffe01d0c1aec14df5120918f346b650f6061ef79
Matt_Tomlinson <mbrucetomlinson@gmail.com>
2017-01-19 19:49:40 -0500

    worked on the detail templates

--- jeopardy/views.py
+++ jeopardy/views.py
@@ -4,8 +4,8 @@ from .models import Clue, Category
 
 
 def index(request):
-    category_list = Category.objects.filter(game=1, round=1)
-    clue_list = Clue.objects.filter(game=1)
+    category_list = Category.objects.filter(game=3, round=1)
+    clue_list = Clue.objects.filter(game=3)
     context = {'clue_list': clue_list, 'category_list': category_list}
     return render(request, 'jeopardy/index.html', context)
 
